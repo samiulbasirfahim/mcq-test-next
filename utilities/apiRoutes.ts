6c8d7273c316bb7177a9b5aea84f07c40be8cc0a
samiul basir fahim <samiulbasirfahim360@gmail.com>
2022-12-20 16:21:00 +0600

    loading spinner issue hosting

--- utilities/apiRoutes.ts
+++ utilities/apiRoutes.ts
@@ -1,5 +1,5 @@
-const baseApi = process.env.enviroment === "dev" ? "http://localhost:3000/api" : "https://quez.vercel.app/api"
-// const baseApi = "http://localhost:3000/api"
+let baseApi = "http://localhost:3000/api";
+
 const getCategory = baseApi + "/category/get"
 const createQuestion = baseApi + "/question/create"
 const getQuestion = baseApi + "/question/get"
